52dec2a2a89bafb35544d39f4739618107152f52
Dmytro <57775385+YevichDmytro@users.noreply.github.com>
2024-05-09 16:10:01 +0300

    upd-1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,1 +1,26 @@
 // У файлі pixabay-api.js зберігай функції для HTTP-запитів.
+
+export function fetchImg(q) {
+  const API_KEY = '43770343-d10c460472ef62dd19f425fcf';
+  const BASE_URL = `https://pixabay.com/api/`;
+
+  const searchParams = new URLSearchParams({
+    key: API_KEY,
+    q,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+
+  return fetch(`${BASE_URL}?${searchParams}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => console.log(error));
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
 // У файлі main.js напиши всю логіку роботи додатка.
+
+import { fetchImg } from './js/pixabay-api.js';
+
+const searchForm = document.querySelector('.form-search-img');
+const input = document.querySelector('.search-input');
+const searchBtn = document.querySelector('.search-btn');
+const sectionBox = document.querySelector('.section-results');
+
+function formHandler(event) {
+  event.preventDefault();
+
+  fetchImg(input.value);
+
+  event.target.reset();
+}
+
+searchForm.addEventListener('submit', formHandler);
